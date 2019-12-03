67cb86086b3c7014d7a7436bf6c89e2cd248efbe
andrew78041 <andrew.micheal@student.guc.edu.eg>
2019-12-03 20:07:32 +0200

    youtube & spotify running

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const { Client } = require('discord.js');
 const { config } = require('dotenv');
-//var SpotifyWebApi = require('spotify-web-api-node');
+var SpotifyWebApi = require('spotify-web-api-node');
 const bot = new Client();
-// const ytdl = require('ytdl-core');
+ const ytdl = require('ytdl-core');
 const Spotify = require('node-spotify-api');
-const keys = require('./keys')
+const keys = require('./keys.js')
 
-// var servers = {};//to save all songs in the queue
+ var servers = {};//to save all songs in the queue
 
 config({
   path: __dirname + '/.env'
 });
 
-// var spotifyApi = new SpotifyWebApi({
-//   clientId: 'e2c29922d0774173bdd62298f476d65a',
-//   clientSecret: '25b915a5b23748c2920930a2cb058027',
-//   redirectUri: 'http://www.example.com/callback'
-// });
+ var spotifyApi = new SpotifyWebApi({
+  clientId: 'e2c29922d0774173bdd62298f476d65a',
+  clientSecret: '25b915a5b23748c2920930a2cb058027',
+  redirectUri: 'http://www.example.com/callback'
+});
 
 var spotify = new Spotify(keys.spotify);
 
@@ -42,61 +42,65 @@ bot.on('message', async (message) => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const cmd = args.shift().toLowerCase();
 
+  let searchTerm= args.toString().replace(","," ");
+
+  console.log(args);
   if(cmd == "hello"){
     const msg = await message.channel.send(`ahlan ya ${message.author.username}`);
   }
-  if(cmd == "play"){
+  if(cmd == "plays"){
 
-    // function play(connection,message){//function to play song
-    //   var server = servers[message.guild.id];
-    //   server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter:"audioonly"}));
-    //   server.queue.shift();
-    //   server.dispatcher.on("end",function(){
-    //     if(server.queue[0]){
-    //       play(connection,message);
-    //     }else{
-    //       connection.disconnect();
-    //     }
-    //   })
+    function play(connection,message){//function to play song
+      var server = servers[message.guild.id];
+      server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter:"audioonly"}));
+      server.queue.shift();
+      server.dispatcher.on("end",function(){
+        if(server.queue[0]){
+          play(connection,message);
+        }else{
+          connection.disconnect();
+        }
+      })
 
-    // }
+    }
 
-    // if(!message.member.voiceChannel){
-    //   const msg = await message.channel.send("You must be in a voice channel!!");
-    //   return;
-    // }
-    // if(!servers[message.guild.id]){
-    //   servers[message.guild.id] = {queue:[]};
-    // }
-    // var server = servers[message.guild.id];
-    // server.queue.push("https://www.youtube.com/watch?v=sZgFubTaqWI");
-    // if(!message.guild.voiceConnection){//to make bot join the voice channel
-    //   message.member.voiceChannel.join().then(function(connection){
-    //     play(connection,message);
-    //   })
-    // }
+    if(!message.member.voiceChannel){
+      const msg = await message.channel.send("You must be in a voice channel!!");
+      return;
+    }
+    if(!servers[message.guild.id]){
+      servers[message.guild.id] = {queue:[]};
+    }
+    var server = servers[message.guild.id];
+    server.queue.push("https://www.youtube.com/watch?v=sZgFubTaqWI");
+    if(!message.guild.voiceConnection){//to make bot join the voice channel
+      message.member.voiceChannel.join().then(function(connection){
+        play(connection,message);
+      })
+    }
   }
   if(cmd == "stop"){
-    // var server = servers[message.guild.id];
-    // if(message.guild.voiceConnection){
-    //   for(var i = server.queue.length-1;i>=0;i--){
-    //     server.queue.splice(i,1);
-    //   }
-    //   server.dispatcher.end();
-    // }
+    var server = servers[message.guild.id];
+    if(message.guild.voiceConnection){
+      for(var i = server.queue.length-1;i>=0;i--){
+        server.queue.splice(i,1);
+      }
+      server.dispatcher.end();
+    }
     if(message.guild.connection) message.guild.voiceConnection.disconnect();
   }
   if(cmd == "skip"){
-    // var server = servers[message.guild.id];
-    // if(server.dispatcher) server.dispatcher.end();
+    var server = servers[message.guild.id];
+    if(server.dispatcher) server.dispatcher.end();
   }
-  if(cmd == "halo"){
-    console.log("before search");
-    spotify.search({type: 'track', query:cmd, limit: '3'})
+
+  if(cmd == "play"){
+   
+    spotify.search({type: 'track', query:searchTerm, limit: '3'})
     .then(function(res){
-      console.log("After then");
+    
       res.tracks.items.forEach(async function(element){
-        const separator = await message.channel.send("cc");
+        const separator = await message.channel.send(".......");
         const artistName = await message.channel.send(`Artist: ${element.album.artists[0].name}`);
         const songName = await message.channel.send(`Song Name: ${element.name}`);
         const previewLink = await message.channel.send(`Spotify Preview: ${element.preview_url}`);
